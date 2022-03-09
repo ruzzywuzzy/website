527dbe7323b7c28f74d8f2e7a07417136e0bde94
ruzzywuzzy <88950207+ruzzywuzzy@users.noreply.github.com>
2022-03-08 21:22:26 -0600

    Add files via upload

---
+++ assets/js/event.js
@@ -0,0 +1,32 @@
+document.onkeypress = function (event) {
+    event = (event || window.event);
+
+    if (event.keyCode == 123 || ((event.keyCode == 73 || event.keyCode == 105) && event.shiftKey && event.ctrlKey)) {
+        return false;
+    }
+    
+    if (event.ctrlKey && (event.keyCode == 65 || event.keyCode == 97)) {
+        return false;
+    }
+}
+
+document.onkeydown = function (event) {
+    event = (event || window.event);
+
+
+    if (event.keyCode == 123 || ((event.keyCode == 73 || event.keyCode == 105) && event.shiftKey && event.ctrlKey)) {
+        return false;
+    }
+
+    if (event.ctrlKey && (event.keyCode == 65 || event.keyCode == 97)) {
+        return false;
+    }
+    
+        if (event.ctrlKey && (event.keyCode == 85)) {
+        return false;
+    }
+}
+
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+document.getElementById("background").setAttribute("draggable", false);
